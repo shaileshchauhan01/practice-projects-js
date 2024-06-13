47ca60c61ac44239572ba99cb03933757755380d
Shailesh Chauhan <114944171+shaileshchauhan01@users.noreply.github.com>
2024-06-13 17:11:25 +0000

    chnage body color

---
+++ project1/javascript.js
@@ -0,0 +1,40 @@
+// const buttons = document.querySelectorAll(".button");
+// const body = document.querySelector("body");
+
+// buttons.forEach(function (button) {
+//   console.log(button);
+//   button.addEventListener("click", function (e) {
+//     console.log(e);
+//     console.log(e.target);
+//     console.log(e.target.id);
+//     if (e.target.id) {
+//       body.style.backgroundColor = e.target.id;
+//     }
+
+    // 2nd way
+
+    // if (e.target.id === "grey") {
+    //   body.style.backgroundColor = e.target.id;
+    // }
+    // if (e.target.id === "white") {
+    //   body.style.backgroundColor = e.target.id;
+    // }
+    // if (e.target.id === "blue") {
+    //   body.style.backgroundColor = e.target.id;
+    // }
+    // if (e.target.id === "yellow") {
+    //   body.style.backgroundColor = e.target.id;
+    // }
+//   });
+// });
+
+// 3rd way
+
+const allButtons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+allButtons.forEach(function (button) {
+  button.addEventListener('mouseover', function (event) {
+    body.style.backgroundColor = button.id;
+  });
+});
